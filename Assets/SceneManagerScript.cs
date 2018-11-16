3057b5daf2d309dab080e5a94035d7491643a841
Huw Tingley <Huw.tingley@gmail.com>
2018-11-16 18:16:01 +0000

    Basic Scenes Implemented

---
+++ Assets/SceneManagerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ChangeScene(int scene_index)
+    {
+        SceneManager.LoadScene(scene_index);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
