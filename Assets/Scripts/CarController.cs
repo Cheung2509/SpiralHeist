04ed4427173016540b2bed696d04d820ccafcbf0
Luke <luke4.roberts@live.uwe.ac.uk>
2018-11-16 18:38:30 +0000

    working player (keyboard)

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,23 +4,104 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    Rigidbody rb;
+    private Rigidbody rb;
+    public GameObject rot;
 
+    public float maxSpeed = 0.0f;
     public float acceleration = 0.0f;
-
+    public float maxRotateSpeed = 0.0f;
+    public float rotation = 0.0f;
+    
+    private float actualAcceleration = 0.0f;
+    private float actualRotation = 0.0f;
 
 	// Use this for initialization
 	void Start ()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();        
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown("w"))
+        #region linear
+
+        // if holding forward
+        if (Input.GetKey("w"))
         {
-            rb.velocity += (transform.forward * acceleration);
+            // if not acceleration fastest
+            if (actualAcceleration < maxSpeed)
+            {
+                // speed up
+                actualAcceleration += acceleration;
+            }
         }
-	}
+        else
+        {
+            // if moving forward
+            if (actualAcceleration > 0.0f)
+            {
+                // slow down
+                actualAcceleration -= acceleration;
+            }
+        }
+
+        #endregion
+
+        #region angular
+
+        // if holding left
+        if (Input.GetKey("a"))
+        {
+            // if 
+            if (actualRotation < maxRotateSpeed)
+            {
+                actualRotation += rotation;
+            }
+        }
+
+        if (Input.GetKey("d"))
+        {
+            // if 
+            if (actualRotation > -maxRotateSpeed)
+            {
+                actualRotation -= rotation;
+            }
+        }
+
+
+        if (!(Input.GetKey("a")) && !(Input.GetKey("d")))
+        {
+            if (actualRotation > 0.1f)
+            {
+                actualRotation -= rotation;
+            }
+            else if(actualRotation < -0.1f)
+            {
+                actualRotation += rotation;
+            }
+            else
+            {
+                actualRotation = 0.0f;
+            }
+        }
+
+        #endregion
+
+        // apply linear motion
+        if (acceleration != 0.0f)
+        {
+            rb.velocity = (transform.forward * actualAcceleration);
+        }
+
+        // apply angular motion
+        if (actualRotation > 0.1f)
+        {
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, rot.transform.rotation, actualRotation);
+        }
+        else if (actualRotation < -0.1f)
+        {
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, rot.transform.rotation, actualRotation);
+        }
+    }     
 }
