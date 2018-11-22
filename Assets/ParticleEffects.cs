73d1f9c5eefde920abeb100a4c5ee36ae7bad0f7
Matthew Cheung <cheung2010@outlook.com>
2018-11-22 14:43:55 +0000

    When players collides with other cars sparks will appear
    Also car collider has been changed to mesh collider to help calculate placement of sparks

---
+++ Assets/ParticleEffects.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffects : MonoBehaviour
+{
+    public ParticleSystem sparks;
+    public CarMovement carController;
+    public MeshCollider boxCollider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        carController = GetComponent<CarMovement>();
+	}
+
+    private void LateUpdate()
+    {
+        //GameObject[] allParticles = GameObject.FindGameObjectsWithTag("Spark");
+
+        //foreach (GameObject obj in allParticles)
+        //{
+        //    ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+        //    if (!ps.IsAlive()|| obj == null)
+        //        Destroy(obj);
+        //}
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            if(carController.carVelocity > 0)
+            {
+                Vector3 pos = contact.point;
+                Quaternion rot = Quaternion.FromToRotation(Vector3.back, contact.normal);
+                ParticleSystem temp = Instantiate(sparks, pos, rot);
+                temp.transform.parent = transform;
+                temp.loop = false;
+                //temp.Emit(1);
+            }
+        }
+    }
+}
