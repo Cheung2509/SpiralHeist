9e66a5bfb4d97bf6a86cd836f7503242707fa26a
Unknown <isaac.mulcahy@yahoo.co.uk>
2018-11-27 19:02:43 +0000

    Intro cutscene
    - News overlay
    - need to allow switch to game

---
+++ Assets/increasePathPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class increasePathPos : MonoBehaviour {
+
+    CinemachineVirtualCamera camera_controller;
+
+	// Use this for initialization
+	void Start () {
+        camera_controller = GetComponent<CinemachineVirtualCamera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        camera_controller.
+
+    }
+}
